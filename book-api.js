2f7595c046980df280000a143c223944c41394fd
Byron Bhuiyan <rafidb8@gmail.com>
2024-05-18 14:23:00 -0700

    Everything up to loadBooks()

---
+++ book-api.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Where we will keep books
+
+let books = [];
+
+app.use(cors());
+
+
+app.post('/book', (req, res) => { 
+   const book = req.body;
+   console.log(book);
+   books.push(book);
+   res.send('Book is added to the database');
+});
+
+app.get('/books', (req,res) => {
+   res.json(books); 
+});
+
+app.listen(port, () => console.log('Hello world app listening on port 3000'));
